fd12ad119803e27dc9579b26599639f82dd5461b
Kiff <125292279+kiffoh@users.noreply.github.com>
2023-10-09 16:50:36 -0500

    Add code in main to determine daily change to side pot

--- src/main.py
+++ src/main.py
@@ -2,3 +2,4 @@ from gmail.read_message import *
 from finances.calculations import *
 from goals.goals import *
 
+print("The amount of side pot collected per each day: ", round(current_side/ days_present,2))
